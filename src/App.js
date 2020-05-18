d53570ae22a3f7d6e9f221e030dc173b49979a94
mirunaen <mirunaen@gmail.com>
2020-05-18 19:15:41 +0200

    get the data and rates ands keys from the API

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import CurrencyRow from "./CurrencyRow";
 const BASE_URL = 'https://api.exchangeratesapi.io/latest';
 
 function App() {
-  //equal to empty string cause we got nothing yet
+  //equal to empty string cause we got any options yet
   const [currencyOptions, setCurrencyOptions] = useState([]);
   useEffect(() => {
     fetch(BASE_URL)
@@ -13,7 +13,11 @@ function App() {
       //to JSON
       .then(res => res.json())
       //which returns another promise that has our data
-      .then(data => console.log(data))
+      .then(data => {
+        //to get the data and get the key portion of the rates
+        //...to destructure the array
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+      })
   }, [])
   //with this empty array we will call useEffect just once
   //because the empty array never changes
