3e441075ec082b02451ff130601e048a9a1d4174
mirunaen <mirunaen@gmail.com>
2020-05-18 19:02:35 +0200

    added the useEffect function

--- src/App.js
+++ src/App.js
@@ -5,22 +5,20 @@ import CurrencyRow from "./CurrencyRow";
 const BASE_URL = 'https://api.exchangeratesapi.io/latest';
 
 function App() {
-  useEffect(() => {
-
-  }, [])
-  //with this empty array we will call useEffect just once
-  //because the empty array never changes
-  //we want to run the useEffect just once
-  return (
-    <>
-      <h1>Convert</h1>
-      <CurrencyRow />
-      <div classNme="equal">
-        =
+}, [])
+//with this empty array we will call useEffect just once
+//because the empty array never changes
+//we want to run the useEffect just once
+return (
+  <>
+    <h1>Convert</h1>
+    <CurrencyRow />
+    <div classNme="equal">
+      =
       </div>
-      <CurrencyRow />
-    </>
-  );
+    <CurrencyRow />
+  </>
+);
 }
 
 export default App;
