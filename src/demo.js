7d69b2c260a3bbbee408ef0f32a6ea72dffcfa56
mirunaen <mirunaen@gmail.com>
2020-05-18 19:38:55 +0200

    D tructure an object

--- src/demo.js
+++ src/demo.js
@@ -1,17 +1,21 @@
-const alphabet = ['A', 'B', 'C', 'D', 'E', 'F']
-const numbers = ['1', '2', '3', '4', '5', '6']
-//const a = alphabet[0]
-//const b = alphabet[1]
-const [a, b, ...rest] = alphabet;
-const newArray = [...alphabet, ...numbers]
-console.log(a);
-console.log(b);
-console.log(rest);
-console.log(newArray);
-
-function sumAndMultiply(a, b) {
-    return [a + b, a * b]
+const personOne = {
+    name: 'Kyle',
+    age: 24,
+    address: {
+        city: 'Somewhere',
+        state: 'One of them'
+    }
 }
 
-const array = sumAndMultiply(2, 3)
-console.log(array);
+const personTwo = {
+    name: 'Sally',
+    age: 23,
+    address: {
+        city: 'Somewhere',
+        state: 'One of them'
+    }
+}
+
+const { name, age } = personOne;
+console.log(name);
+console.log(age);
