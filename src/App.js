e00bb250020d299021b79180b5a4c968c0293d95
mirunaen <mirunaen@gmail.com>
2020-05-18 19:08:48 +0200

    added a useState to set the currency options

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import CurrencyRow from "./CurrencyRow";
 
 const BASE_URL = 'https://api.exchangeratesapi.io/latest';
 
 function App() {
+  //equal to empty string cause we got nothing yet
+  const [currencyOptions, setCurrencyOptions] = useState([]);
   useEffect(() => {
     fetch(BASE_URL)
+      //when we get the fetch back we convert the response
+      //to JSON
       .then(res => res.json())
+      //which returns another promise that has our data
       .then(data => console.log(data))
   }, [])
   //with this empty array we will call useEffect just once
